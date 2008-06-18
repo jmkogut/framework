b05ab9e454ea1e73fd2dff157f0f55bda15d3edb
root <joshua.kogut@gmail.com>
2008-06-18 10:39:58 -0700

    Base controller

---
+++ Controller.py
@@ -0,0 +1,16 @@
+from webob import Request, Response
+from webob import exc
+
+def controller(func):
+    def replacement(environ, start_resonse):
+        req = Request(environ)
+        try:
+            resp = func(req, **req.urlvars):
+        except exc.HTTPException, e:
+            resp = e
+        
+        if isinstance(resp, basestring):
+            resp = Response(body=resp)
+
+        return resp(environ, start_response)
+    return replacement
