6ef57722e1d9b67cc62033c612b79eb72eb216d8
root <joshua.kogut@gmail.com>
2008-06-18 09:18:14 -0700

    Basic Routes configuration and testing for routes

---
+++ Config/Routes.py
@@ -0,0 +1,11 @@
+"""
+    Routes configuration files, it's a regex:controller pair, pretty
+    simple, really. I promise.
+"""
+
+routes = {
+    '/': 'controllers:index',
+
+    '/(?P<year>\d\d\d\d)/': 'controllers:archive',             # controllers:archive would be passed a four digit year
+    '/(?P<year>\d\d\d\d)/(?P<name>\w+)/': 'controllers:person', # controllers:person would be passed the year and person
+}
---
+++ Router.py
@@ -0,0 +1,9 @@
+from Config import Routes
+
+import re
+
+
+class Router():
+    
+    def __init__(self):
+        self.routes = Config.Routes.routes
