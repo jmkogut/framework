d1fc0ec298dc146953009246f77ded201ee50c4e
Joshua Michael Kogut <joshua@karai.dottru.net>
2008-06-20 19:37:34 -0700

    Small fixes

---
+++ Controllers.py
@@ -0,0 +1,43 @@
+from webob import Request, Response
+from webob import exc
+
+def Controller(func):
+    def replacement(environ, start_response):
+        req = Request(environ)
+        try:
+            resp = func(req, **req.urlvars)
+        except exc.HTTPException, e:
+            resp = e
+        
+        if isinstance(resp, basestring):
+            resp = Response(body=resp)
+
+        return resp(environ, start_response)
+    return replacement
+
+def RestController(cls):
+    def replacement(environ, start_response):
+        req = Request(environ)
+
+        try:
+            instance = cls(req, **req.urlvars)
+            action = req.urlvars.get('action')
+
+            if action:
+                action += '_' + req.method.lower()
+            else:
+                action  = req.method.lower()
+            
+            try:
+                method = getattr(instance, action)
+            except AttributeError:
+                raise exc.HTTPNotFound("No action %s" % action)
+
+            resp = method()
+            if isinstance(resp, basestring):
+                resp = Response(body=resp)
+        except exc.HTTPException, e:
+            resp = e
+
+        return resp(environ, start_response)
+    return replacement
---
+++ RestHello.py
@@ -0,0 +1,16 @@
+from Controllers import RestController
+
+class _Hello:
+
+    def __init__(self, req):
+        self.request = req
+
+    def get(self):
+        return '''<form method="POST">Your Name
+                    <input type="text" name="name">
+                    <input type="submit"></form>'''
+    
+    def post(self):
+        return 'Hello %s! The resident dipshit!' % self.request.params['name']
+
+Hello = RestController(_Hello)
